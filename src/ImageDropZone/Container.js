4d80f76b605c1c6e7b39b66210254cc2510fa549
liuderchi <liuderchi@gmail.com>
2018-10-27 22:05:15 +0800

    update flow

--- src/ImageDropZone/Container.js
+++ src/ImageDropZone/Container.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import P from '../P';
+import { type ThemeProps } from '../utils/type.flow';
 
 type Props = {
   src?: string,
@@ -16,19 +17,20 @@ const Container: React.ComponentType<Props> = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: ${props => props.height}px;
+  height: ${(props: Props) => props.height}px;
   padding: 12px;
   text-align: center;
   box-sizing: border-box;
 
-  background-color: ${props => props.theme.color.grayLight};
-  border: 1px ${props => (props.src && !props.canDrop ? 'solid' : 'dashed')}
-    ${props =>
+  background-color: ${({ theme }: ThemeProps) => theme.color.grayLight};
+  border: 1px
+    ${(props: Props) => (props.src && !props.canDrop ? 'solid' : 'dashed')}
+    ${(props: Props & ThemeProps) =>
       props.canDrop ? props.theme.color.primary : props.theme.color.grayBase};
   border-radius: 3px;
   transition: all cubic-bezier(0.47, 0, 0.75, 0.72) 0.3s;
 
-  ${props =>
+  ${(props: Props) =>
     props.src &&
     css`
       background-image: url("${props.src}");
