29de36853d57ba4c95f994bda0c076456b637475
Michael Hsu <evenchange4@gmail.com>
2018-11-12 15:58:16 +0800

    feat(InputGroup): add span for display only (#54)
    * feat(InputGroup): add span for unit
    
    * update

--- src/InputGroup/InputGroup.js
+++ src/InputGroup/InputGroup.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react';
 import styled from 'styled-components';
+import { type ThemeProps } from '../utils/type.flow';
 
 export type Props = {};
 
@@ -41,6 +42,36 @@ const InputGroup: React.ComponentType<Props> = styled.div`
   > *:last-child {
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
+  }
+
+  /**
+   * Unit
+   */
+
+  > span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+    padding: 0 5px;
+    min-height: ${({ theme }: ThemeProps) => theme.height.normal};
+    min-width: ${({ theme }: ThemeProps) => theme.height.normal};
+    border-radius: 3px;
+    border: 1px solid ${({ theme }) => theme.color.grayBase};
+    background-color: ${({ theme }) => theme.color.white};
+    color: ${({ theme }) => theme.color.grayDark};
+  }
+  > * + span {
+    border-left-width: 0;
+  }
+  > * + span:not(:last-child) {
+    border-right-width: 0;
+  }
+  > span:first-child {
+    border-right-width: 0;
+  }
+  > span:last-child {
+    border-left-width: 0;
   }
 `;
 
