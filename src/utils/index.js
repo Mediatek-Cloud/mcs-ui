59c4c4d4d5b41312f6853b42a8835e84f76ab9d9
Michael Hsu (徐承志) <evenchange4@gmail.com>
2018-12-10 15:42:37 +0800

    chore: expose all utils for es modules usage

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+export * from './darken';
+export * from './theme';
+
+export { default as emptyFunction } from './emptyFunction';
+export { default as getCurrentYear } from './getCurrentYear';
+export { default as isString } from './isString';
+export { default as localTimeFormat } from './localTimeFormat';
+export { default as opacity } from './opacity';
+export { default as toMB } from './toMB';
